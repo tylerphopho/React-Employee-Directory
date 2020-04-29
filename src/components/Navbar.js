835f6298473e6bef9b36ffa788bc437fb72bb899
tylerphopho <tyler.phopho@gmail.com>
2020-04-28 19:19:27 -0600

    created Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Navbar() {
+    return(
+        <nav className="nav-wrapper grey darken-4">
+            <div className="container">
+                <div className="brand-logo center">Employee Directory</div>
+                
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
