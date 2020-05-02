4187abfee1c95cd6d43010755d1b32a330cc4289
tylerphopho <tyler.phopho@gmail.com>
2020-05-02 15:48:08 -0600

    ran react-build script

---
+++ client/build/precache-manifest.eb5a86b688a071e151176a73d574292b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83b740d1e6405f0b517d4da0847eabcb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b59677b62b688fb25ac5",
+    "url": "/static/css/2.0eec7bad.chunk.css"
+  },
+  {
+    "revision": "b59677b62b688fb25ac5",
+    "url": "/static/js/2.d16ea806.chunk.js"
+  },
+  {
+    "revision": "b3924902b6517db7f2b56facf8ba237b",
+    "url": "/static/js/2.d16ea806.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ede5920fc46616e18db",
+    "url": "/static/js/main.636e2bd4.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-
-const morgan = require("morgan");
+const path = require("path")
+const morgan = require("morgan");;
 const mongoose = require("mongoose");
 const apiRouting = require("./routes/apiRouting")
 const cors = require("cors")
